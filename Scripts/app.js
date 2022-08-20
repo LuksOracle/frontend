e32b350633e7511f996d41050d9b33091331d7db
~MarcusWentz <markzcollege404@gmail.com>
2022-08-20 16:43:42 -0400

    LINK balance now added with ERC20 interface

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,7 +6,6 @@ let accounts = [];
 document.getElementById("getCurrentAccountConnected").innerHTML =  "None. Please click the top button to connect."
 document.getElementById("getValueStateSmartContract").innerHTML =  "Please connect wallet first to check withdrawal time."
 
-
 //If Metamask is not detected the user will be told to install Metamask.
 function detectMetamaskInstalled(){
   try{
@@ -35,10 +34,11 @@ function enableMetamaskOnLuksoL16() {
   }
 }
 
+
 //Get the latest value.
 function checkLastDateWithdrawn() {
   contractDefined_JS.methods.userPreviousWithdrawTime(accounts[0]).call((err, balance) => {
-    
+
     if(balance === undefined){
       document.getElementById("getValueStateSmartContract").innerHTML =  "Install Metamask and select LuksoL16 Testnet to have a Web3 provider to read blockchain data."
     }
@@ -49,7 +49,7 @@ function checkLastDateWithdrawn() {
       }
       else {
         document.getElementById("getValueStateSmartContract").innerHTML = "You have not yet withdrawn from the Lukso L16 LINK faucet."
-      } 
+      }
     }
   })}
 
@@ -75,9 +75,19 @@ const web3 = new Web3(window.ethereum)
 const contractAddress_JS = '0xe33EE68Fc5477Ea95F4897b67d3E763b7F74FC52'
 const contractABI_JS = [{"anonymous":false,"inputs":[],"name":"faucetWithdraw","type":"event"},{"inputs":[],"name":"withdrawDirect","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"relayCaller","type":"address"}],"name":"withdrawRelay","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"relayAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userPreviousWithdrawTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
-// [{"anonymous":false,"inputs":[],"name":"setEvent","type":"event"},{"inputs":[{"internalType":"uint256","name":"x","type":"uint256"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"storedData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
-
 const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
+
+const chainlinkInterfaceERC20_ADDRESS = '0xbFB26279a9D28CeC1F781808Da89eFbBfE2c4268'
+const chainlinkInterfaceERC20_ABI =
+[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+const chainlinkInterfaceERC20_CONTRACT = new web3.eth.Contract(chainlinkInterfaceERC20_ABI, chainlinkInterfaceERC20_ADDRESS)
+
+
+//LINK BALANCE
+chainlinkInterfaceERC20_CONTRACT.methods.balanceOf(contractAddress_JS).call((err, contractLINKbalanceResult) => {
+  document.getElementById("getFaucetLinkBalance").innerHTML = contractLINKbalanceResult/(10**18) + " LINK"
+});
+
 
 // MODIFY CONTRACT STATE WITH SET FUNCTION WITH PREDEFINED DATA FROM WEB3.JS
 const changeStateInContractEvent = document.querySelector('.changeStateInContractEvent');
@@ -105,36 +115,19 @@ changeStateInContractEvent.addEventListener('click', () => {
 });
 
 //Get the latest event. Once the event is triggered, website will update value.
-contractDefined_JS.events.setEvent({
+contractDefined_JS.events.faucetWithdraw({
      fromBlock: 'latest'
  }, function(error, eventResult){})
  .on('data', function(eventResult){
    console.log(eventResult)
    //Call the get function to get the most accurate present state for the value.
   checkLastDateWithdrawn()
+  //LINK BALANCE
+  chainlinkInterfaceERC20_CONTRACT.methods.balanceOf(contractAddress_JS).call((err, contractLINKbalanceResult) => {
+    document.getElementById("getFaucetLinkBalance").innerHTML = contractLINKbalanceResult/(10**18) + " LINK"
+  });
    })
  .on('changed', function(eventResult){
      // remove event from local database
  })
  .on('error', console.error);
-
- //Changing the integer state in a function which will fire off an event.
- //Make sure values are in hex or Metamask will fail to load.
- //DO NOT SET A VALUE UNLESS THE CONTRACT NEEDS IT FOR MSG.VALUE REQUIRE STATEMENTS
- const sendEthButton = document.querySelector('.sendEthButton');
- sendEthButton.addEventListener('click', () => {
-   checkAddressMissingMetamask()
-   ethereum
-     .request({
-       method: 'eth_sendTransaction',
-       params: [
-         {
-           from: accounts[0],
-           to: '0xc1202e7d42655F23097476f6D48006fE56d38d4f',
-           value: '0x29a2241af62c0',
-         },
-       ],
-     })
-     .then((txHash) => console.log(txHash))
-     .catch((error) => console.error);
- });
