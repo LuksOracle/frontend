877d75224ae8f7eb3397af4b5e05927050bcfb42
Marco Germanier <marco@mrgt.me>
2022-08-27 11:48:22 +0100

    fixed faucet.html bug
    page was refreshing on request, thus disconnecting metamask.

--- src/scripts/faucet.js
+++ src/scripts/faucet.js
@@ -13,17 +13,17 @@ function detectMetamaskInstalled(){
 
 //Alert user to connect their Metamask address to the site before doing any transactions.
 function checkAddressMissingMetamask() {
-  console.log(accounts.length)
   if(accounts.length == 0) {
     alert("No address from Metamask found. Click the top button to connect your Metamask account then try again without refreshing the page.")
   }
 }
 
-//Function called for getting Metamask accounts on LuksoL16. Used in every button in case the user forgets to click the top button.
+// Function called for getting Metamask accounts on LuksoL16. 
+// Used in every button in case the user forgets to click the top button.
 function enableMetamaskOnLuksoL16() {
-  //Get account details from Metamask wallet.
+  // Get account details from Metamask wallet.
   getAccount();
-  //Check if user is on the LuksoL16 testnet. If not, alert them to change to LuksoL16.
+  // Check if user is on the LuksoL16 testnet. If not, alert them to change to LuksoL16.
   if(window.ethereum.networkVersion != 2828){
     alert("You are not on the LuksoL16 Testnet! Please switch to LuksoL16 and refresh page.")
     console.log(window.ethereum.networkVersion)
@@ -37,6 +37,7 @@ function checkLastDateWithdrawn() {
     if(balance === undefined){
       document.getElementById("getValueStateSmartContract").innerHTML =  "Install Metamask and select LuksoL16 Testnet to have a Web3 provider to read blockchain data."
     }
+    
     else{
       if (balance != 0) {
         balance = new Date(balance * 1000).toLocaleString()
@@ -78,23 +79,26 @@ ethereumButton.addEventListener('click', () => {
 //Make Metamask the client side Web3 provider. Needed for tracking live events.
 const web3 = new Web3(window.ethereum)
 
-//Now build the contract with Web3.
+//Now build the contracts with Web3.
+
+// REQUEST LINK FROM FAUCET
 const contractAddress_JS = '0xe33EE68Fc5477Ea95F4897b67d3E763b7F74FC52'
 const contractABI_JS = [{"anonymous":false,"inputs":[],"name":"faucetWithdraw","type":"event"},{"inputs":[],"name":"withdrawDirect","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"relayCaller","type":"address"}],"name":"withdrawRelay","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"relayAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userPreviousWithdrawTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
-
 const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
 
+// CHAINLINK FAUCET BALANCE
 const chainlinkInterfaceERC20_ADDRESS = '0xbFB26279a9D28CeC1F781808Da89eFbBfE2c4268'
 const chainlinkInterfaceERC20_ABI =
 [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 const chainlinkInterfaceERC20_CONTRACT = new web3.eth.Contract(chainlinkInterfaceERC20_ABI, chainlinkInterfaceERC20_ADDRESS)
+
+// FUNCTIONS TO RETRIEVE SMART CONTRACT DATA
 
 //LINK BALANCE
 chainlinkInterfaceERC20_CONTRACT.methods.balanceOf(contractAddress_JS).call((err, contractLINKbalanceResult) => {
   if ((contractLINKbalanceResult/(10**18)) == 0){document.getElementById("getFaucetLinkBalance").innerHTML = "The faucet is empty!"}
   else {document.getElementById("getFaucetLinkBalance").innerHTML = contractLINKbalanceResult/(10**18) + " LINK"}
 });
-
 
 // MODIFY CONTRACT STATE WITH SET FUNCTION WITH PREDEFINED DATA FROM WEB3.JS
 const changeStateInContractEvent = document.querySelector('.changeStateInContractEvent');
@@ -119,6 +123,7 @@ changeStateInContractEvent.addEventListener('click', () => {
     })
     .then((txHash) => console.log(txHash))
     .catch((error) => console.error);
+  
 });
 
 //Get the latest event. Once the event is triggered, website will update value.
